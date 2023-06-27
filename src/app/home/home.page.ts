d33591023495cd5d3f7ff797c0caf425663de201
Pedrinduarte <118394334+Pedrinduarte@users.noreply.github.com>
2023-06-27 19:30:36 +0000

    terminado calculadora

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,8 +6,35 @@ import { IonicModule } from '@ionic/angular';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule,],
 })
 export class HomePage {
-  constructor() {}
+  resultado: number = 0;
+      exibir(numero: number) {
+      let res = document.getElementById('resultado')!.innerHTML; 
+      document.getElementById('resultado')!.innerHTML += numero;
+       this.resultado = numero * 1; 
+      } 
+      exibirOP(op: string){
+         let res = document.getElementById('resultado')!.innerHTML; 
+         document.getElementById('resultado')!.innerHTML += op;
+         } 
+         clear(){ document.getElementById('resultado')!.innerHTML = ""; 
+        }
+        back() {
+          const element = document.getElementById('resultado');
+          if (element) {
+            const res = element.textContent!;
+            element.textContent = res.slice(0, -1);
+          }
+        }
+        
+        calcular() {
+          let res = document.getElementById('resultado')!.innerHTML;
+          if (res) {
+            document.getElementById('resultado')!.innerHTML = eval(res);
+          }
+        }
+      
 }
+
